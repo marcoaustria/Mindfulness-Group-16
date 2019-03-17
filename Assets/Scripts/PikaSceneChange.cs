9712e91ffa0c1a49a838ea8dd81fb7a3c89cb48d
Caleb Reed <calebreed@ufl.edu>
2019-03-16 20:13:15 -0400

    added walk to outside scene and kitchen scene, pikachu to outside scene, scene transition to kitchen scene by clicking on pikachu

---
+++ Assets/Scripts/PikaSceneChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PikaSceneChange : MonoBehaviour
+{
+    private bool tracked;
+    // Start is called before the first frame update
+    public string sceneName;
+    void Start()
+    {
+        tracked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tracked && Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    public void pointerEntered(){
+
+        tracked = true;
+    }
+
+    public void pointerExited()
+    {
+        tracked = false;
+    }
+
+
+}
---
+++ Assets/Scripts/VRWalk.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRWalk : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private CharacterController cc;
+    private Transform vrCamera;
+
+    private bool isWalking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        vrCamera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void movePlayer()
+    {
+
+    }
+}
